b41cd5418976407cd28b7351b2a108366ea0a279
miroszlv <miroszlv@users.noreply.github.com>
2018-05-10 20:36:13 +0200

    Part 3

---
+++ miroszlv/week 01/D04/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Joo Mirko");
+        System.out.println(27);
+        System.out.println(1.75);
+        }
+}
---
+++ miroszlv/week 01/D04/src/TwoNumbers.java
@@ -0,0 +1,23 @@
+public class TwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 13;
+        a += 22;
+        System.out.println(a);
+
+        int b = 22;
+        b -= 13;
+        System.out.println(b);
+
+        int c = 22;
+        c *= 13;
+        System.out.println(c);
+
+        int d = 22;
+        d /= 13;
+        System.out.println(d);
+
+        
+    }
+}
