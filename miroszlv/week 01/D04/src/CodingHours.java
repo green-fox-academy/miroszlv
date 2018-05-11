018708ce7147e8145921b691110a07e10b2a4381
miroszlv <miroszlv@users.noreply.github.com>
2018-05-11 11:46:25 +0200

    Favorite Number

---
+++ miroszlv/week 01/D04/src/CodingHours.java
@@ -0,0 +1,17 @@
+public class CodingHours
+{
+
+    public static void main(String[] args) {
+
+        int a = 6;
+            a *= 85;
+
+        System.out.println(a);
+
+        int b = 58;
+            b /= 52;
+        int
+        System.out.println(c);
+    }
+
+}
--- miroszlv/week 01/D04/src/TwoNumbers.java
+++ miroszlv/week 01/D04/src/TwoNumbers.java
@@ -18,6 +18,6 @@ public class TwoNumbers {
         d /= 13;
         System.out.println(d);
 
-        
+
     }
 }
